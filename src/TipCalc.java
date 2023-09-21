28ca54f0a13068ebfa588a5a6ad863a14ca2a3cb
BT_1N3_08 <sample@bths.edu>
2023-09-21 10:27:45 -0400

    initial comment

--- src/TipCalc.java
+++ src/TipCalc.java
@@ -4,16 +4,12 @@ public class TipCalc {
         Scanner scan = new Scanner(System.in);
         System.out.print("Welcome to the tip calculator");
         System.out.println("How many people are in your group: ");
-        int people = scan.nextInt();
-        scan.nextLine;
+        //int people = scan.nextInt();
         System.out.println("What's the tip percentage? (0-100): ");
-        double percent = scan.nextInt();
+        //double percent = scan.nextInt();
 
+        System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): ");
 
-        while (number != 1) {
-            System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): ");
-            number = scan.nextInt();
-        }
 
         System.out.print("Done!");
     }
